5a6bb45b12cd99e10f4187834d6ea642b5770068
James Young <james.young@turner.com>
2013-01-29 17:56:14 -0500

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
-dust-compiler
-=============
+# Dust Compiler
+This as a basic dust compiler, there are many out there. This one adds support
+for OS X 10.8.x Notification Center to bring more visiblity to compile errors
+when developing while the terminal window that it is running in is not visible.
+
+
+## Install
+You will likely integrate this into the project you are working on.  Place the
+`package.json` in the root of your project folder and run `npm install`.  This
+will install the other node packages needed.
+
+    $ npm install
+
+Next put `dust-compiler.js` anywhere you like in your project folder.  Edit the
+file and set the `source` and `destination` paths on where it should watch for
+dust files (source), and where it should write compiled js files (destination).
+
+
+## Turning it on
+To fire it up just do this:
+
+    $ node dust-compiler.js
+
+Notifications will look like this:
+
+Success!
+
+![](http://grab.by/jrgo)
+
+or Failure :(
+
+![](http://grab.by/jrh4)
---
+++ dust-compiler.js
@@ -0,0 +1,79 @@
+/*jslint node: true */
+
+/*
+ * dust-compiler.js
+ * james.young@turner.com
+ *
+ * This as a basic dust compiler, there are many out there. This one adds support
+ * for OS X 10.8.x Notification Center to bring more visiblity to compile errors
+ * when developing while the terminal window that it is running in is not visible.
+ *
+ * Usage from terminal:
+ *     node dust-compiler.js
+ */
+
+
+var source = "./path/to/source/dust/files/",
+    destination = "./path/to/destination/dust/files/",
+    fs = require('fs'),
+    dust = require('dustjs-linkedin'),
+    watch = require('watch'),
+    notifier = require('terminal-notifier');
+
+
+function log(message) {
+    'use strict';
+
+    if (process.platform === 'darwin') {
+        notifier(message, {
+            title: 'Dust Compiler',
+            activate: 'com.apple.Terminal'
+        });
+    }
+
+    console.log(message);
+}
+
+
+function compile(path, curr, prev) {
+    'use strict';
+
+    var filename = path.split('/').reverse()[0].replace('.dust', ''),
+        filepath = destination + filename + '.js',
+        error = false,
+        data;
+
+    fs.readFile(path, function (err, data) {
+        if (err) {
+            log('fs.readFile error');
+            throw err;
+        }
+
+        try {
+            data = dust.compile(data.toString(), filename);
+        } catch (e) {
+            error = true;
+            log(e);
+        }
+
+        if (!error) {
+            fs.writeFile(filepath, data, function (err) {
+                if (err) {
+                    log('fs.writeFile error: ' + err);
+                    throw err;
+                }
+                log('Saved ' + filepath);
+            });
+        }
+    });
+}
+
+
+watch.createMonitor(source, function (monitor) {
+    'use strict';
+
+    log('Watching' + source);
+    monitor.files['*.dust', '*/*'];
+    monitor.on('created', compile);
+    monitor.on('changed', compile);
+});
