59b4068aa1985d85932f2f519627b5cf217eeee6
James Young <james.young@turner.com>
2013-04-18 22:44:08 -0400

    code refactoring

--- dust-compiler.js
+++ dust-compiler.js
@@ -1,61 +1,48 @@
 #! /usr/bin/env node
 /*jslint node: true */
 
-/*
- * dust-compiler
- * jmeyoung@gmail.com
- *
- * This is a basic dust compiler, there are many out there. This one adds support
- * for OS X 10.8.x Notification Center to bring more visiblity to compile errors
- * when developing while the terminal window that it is running in is not visible.
- *
- * Usage from terminal for compiling when files change:
- *     ./dust-compiler
- *
- * If you need to compile lots of dust templates for the first time and don't
- * want to start up the compiler then go touch every single file, do this:
- *     ./dust-compiler --bootstrap
- */
+'use strict';
 
-
-var source = "src/main/dust-templates/",             // must end in slash
-    destination = "src/main/js/lib/dust-templates/", // must end in slash
+var source = 'change-me/',      // must end in slash
+    destination = 'change-me/', // must end in slash
     fs = require('fs'),
     path = require('path'),
-    mkdirp = require('mkdirp'),
     dust = require('dustjs-linkedin'),
     watch = require('watch'),
     wrench = require('wrench'),
-    log = (function () {
-        "use strict";
-        var notifier;
-
-        // Determine the most-appropriate notifier for the platform.
-        switch (process.platform) {
-        case "darwin":
-            notifier = require("terminal-notifier");
-            return function (message) {
-                notifier(message, {
-                    title: 'Dust Compiler',
-                    activate: 'com.apple.Terminal'
-                });
-                console.log(message);
-            };
-        case "linux":
-            notifier = require("notify-send");
-            return function (message) {
-                notifier.notify("Dust Compiler", message);
-                console.log(message);
-            };
-        default:
-            return console.log;
-        }
-    }()),
+    mkdirp = require('mkdirp'),
     bootstrap = false;
 
-function compile(src, curr, prev) {
-    'use strict';
 
+function log() {
+    var notifier;
+
+    // Determine the most-appropriate notifier for the platform.
+    switch (process.platform) {
+    case 'darwin':
+        notifier = require('terminal-notifier');
+        return function (message) {
+            notifier(message, {
+                title: 'Dust Compiler',
+                activate: 'com.apple.Terminal'
+            });
+            console.log(message);
+        };
+
+    case 'linux':
+        notifier = require('notify-send');
+        return function (message) {
+            notifier.notify('Dust Compiler', message);
+            console.log(message);
+        };
+
+    default:
+        return console.log;
+    }
+}
+
+
+function compile(src, curr, prev) {
     var filename,
         filepath,
         error = false,
@@ -63,18 +50,17 @@ function compile(src, curr, prev) {
 
     src = path.normalize(src);
 
-    if (path.extname(src) === ".dust") {
+    if (path.extname(src) === '.dust') {
         fs.stat(src, function (err, stat) {
             if (err) {
-                log("fs.stat error");
+                log('fs.stat error');
                 throw err;
             }
 
             if (!stat.isDirectory()) {
 
                 filename = src.substring(source.length);
-                filepath = destination +
-                    filename.substring(0, filename.length - 5) + ".js";
+                filepath = destination + filename.substring(0, filename.length - 5) + '.js';
 
                 fs.readFile(src, function (err, data) {
                     if (err) {
@@ -92,7 +78,7 @@ function compile(src, curr, prev) {
                     if (!error) {
                         mkdirp.mkdirp(path.dirname(filepath), function (err) {
                             if (err) {
-                                log("mkdirp.mkdirp error: " + err);
+                                log('mkdirp.mkdirp error: ' + err);
                                 throw err;
                             }
 
@@ -113,18 +99,17 @@ function compile(src, curr, prev) {
 
 
 process.argv.forEach(function (arg, idx) {
-    "use strict";
     if (idx < 2) { return; }
 
     switch (arg) {
-    case "--bootstrap":
+    case '--bootstrap':
         bootstrap = true;
         break;
-    case "--no-notify":
+    case '--no-notify':
         log = console.log;
         break;
     default:
-        log("Ignoring unrecognized option: " + arg);
+        log('Ignoring unrecognized option: ' + arg);
     }
 });
 
@@ -133,8 +118,6 @@ destination = path.normalize(destination);
 
 if (bootstrap) {
     wrench.readdirRecursive(source, function (error, fileList) {
-        'use strict';
-
         if (fileList) {
             fileList.forEach(function (filename) {
                 compile(source + filename);
@@ -143,8 +126,6 @@ if (bootstrap) {
     });
 } else {
     watch.createMonitor(source, function (monitor) {
-        'use strict';
-
         log('Watching ' + source);
         monitor.on('created', compile);
         monitor.on('changed', compile);
