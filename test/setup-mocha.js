651c0b7de471ad532bea1baa72f104c58f22889f
James Young <james.young@turner.com>
2013-10-06 20:47:39 -0400

    basic mocha setup, coverage commented out

--- test/setup-mocha.js
+++ test/setup-mocha.js
@@ -1,10 +1,32 @@
 'use strict';
 
+/*
+ * This file is required by the mocha-test grunt task.  This should contain
+ * things that need to be done for every test file
+ */
+
+var chai = require('chai');
+
+// should must be defined globally to make the linter happy
+global.should = chai.should();
+
+// include stack track on test failure
+chai.Assertion.includeStack = false;
+
+// needed for blanket instrumentation with mocha
+// require('blanket')({
+//     'data-cover-only': ['cli-wrapper.js', 'process.js'],
+//     'data-cover-never': ['node_modules', 'test']
+// });
+
+
+// ---------------------
+
 // require('blanket')({
 //     'data-cover-only': [ 'src/' ],
 //     'data-cover-never': ['node_modules', 'test']
 // });
-require('blanket')({
+// require('blanket')({
     // Only files that match the pattern will be instrumented
-    pattern: '/src/'
-});
+//     pattern: '/src/'
+// });
