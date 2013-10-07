2c1719c517bddfa251f8c91cf8a74bfe9cc65765
James Young <james.young@turner.com>
2013-10-06 23:46:30 -0400

    this is just a placeholder now

--- test/mocha/dust-compiler.test.js
+++ test/mocha/dust-compiler.test.js
@@ -1,12 +1,12 @@
 /* global describe, it */
 'use strict';
 
-var dustCompiler = require('../../lib/dust-compiler.js');
+// var dustCompiler = require('../../lib/dust-compiler.js')('one', 'tw');
 
 // { foo: { "bar": "foobar", "baz": "foobaz" } }
 
 describe.skip('dust-compiler', function () {
-    it('should ...', function (done) {
+    it('should ...', function () {
 
     });
 });
