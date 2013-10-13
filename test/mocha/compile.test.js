e2b3b5492e5e682edf5b8d6152dd9e2349d96e3e
James Young <james.young@turner.com>
2013-10-12 23:29:03 -0400

    added unit test

--- test/mocha/compile.test.js
+++ test/mocha/compile.test.js
@@ -11,7 +11,7 @@ describe('compile', function () {
         // the mocha tests.  The default path when no path is provided is '.'
         compile('test.dust', './test/mock/');
 
-        // wait 1.5 seconds before reading the file
+        // wait 0.5 seconds before reading the file
         setTimeout(function () {
             fs.readFile('test.js', 'utf-8', function (error, data) {
                 if (error) { console.log(error); }
@@ -20,13 +20,13 @@ describe('compile', function () {
                 data.should.contain('dust.register("test"');
                 done();
             });
-        }, 1500);
+        }, 500);
     });
 
     it('should compile a template with a source path, filename, and destination path provided', function (done) {
         compile('test.dust', './test/mock/', './test/mock/');
 
-        // wait 1.5 seconds before reading the file
+        // wait 0.5 seconds before reading the file
         setTimeout(function () {
             fs.readFile('./test/mock/test.js', 'utf-8', function (error, data) {
                 if (error) { console.log(error); }
@@ -35,13 +35,13 @@ describe('compile', function () {
                 data.should.contain('dust.register("test"');
                 done();
             });
-        }, 1500);
+        }, 500);
     });
 
     it('should compile a template and default to NOT include the path in the registered name if options.includepath is not set', function (done) {
         compile('test.dust', './test/mock/', './test/mock/');
 
-        // wait 1.5 seconds before reading the file
+        // wait 0.5 seconds before reading the file
         setTimeout(function () {
             fs.readFile('./test/mock/test.js', 'utf-8', function (error, data) {
                 if (error) { console.log(error); }
@@ -50,13 +50,13 @@ describe('compile', function () {
                 data.should.contain('dust.register("test"');
                 done();
             });
-        }, 1500);
+        }, 500);
     });
 
     it('should include the path as the registered dust template name if options.includepath is set', function (done) {
         compile('test.dust', './test/mock/', './test/mock/', { includepath: true });
 
-        // wait 1.5 seconds before reading the file
+        // wait 0.5 seconds before reading the file
         setTimeout(function () {
             fs.readFile('./test/mock/test.js', 'utf-8', function (error, data) {
                 if (error) { console.log(error); }
@@ -65,6 +65,13 @@ describe('compile', function () {
                 data.should.contain('dust.register("./test/mock/test"');
                 done();
             });
-        }, 1500);
+        }, 500);
+    });
+
+    it('should not use log-notify if options.nonotify is set', function () {
+        // this test is satisified by the code coverage showing the conditional is tested
+        // there is no way to use a 'should', 'expect', or 'assert' to check if the os specific
+        // notification was not used.
+        compile('test.dust', './test/mock/', './test/mock/', { nonotify: true });
     });
 });
